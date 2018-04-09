6ac1190c46914eea8c21f123911d42b8829eb5dd
Wesley Hutchinson <w.hutchinson@lancaster.ac.uk>
2018-04-09 22:47:49 +0100

    Databinding and dataconverter

---
+++ DataCoverterDemo/Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCoverterDemo
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private DateTime _startDate;
+        private string name;
+        private string title;
+
+
+
+        public DateTime StartDate
+        {
+            get { return _startDate; }
+            set
+            {
+                _startDate = value;
+                OnPropertyChanged();
+            }
+        }
+      
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public Employee( string name, string title, DateTime startDate)
+        {
+            Name = name;
+            Title = title;
+            StartDate = startDate;
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string caller = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(caller));
+            }
+        }
+    }
+}
